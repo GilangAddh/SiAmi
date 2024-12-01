ab621888de25d41e92f2d0bb6599a042fb96c73a
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2024-12-01 18:11:30 +0700

    fix search

--- resources/views/livewire/manajemen-pengguna.blade.php
+++ resources/views/livewire/manajemen-pengguna.blade.php
@@ -16,8 +16,8 @@
 
     <div class="flex justify-between my-6 items-center">
         <label class="input input-bordered flex items-center input-sm py-5 pr-4 pl-1 w-3/5 md:w-1/4">
-            <input wire:model.debounce.300ms="search" type="text" class="grow border-none text-sm gap-2 w-full"
-                placeholder="Cari" />
+            <input wire:model.live="search" type="text" class="grow border-none text-sm gap-2 w-full"
+                placeholder="Cari nama atau email" />
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="h-4 w-5 opacity-70">
                 <path fill-rule="evenodd"
                     d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
