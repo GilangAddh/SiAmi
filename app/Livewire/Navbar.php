de058e174b1feb7ce2edb8e75264ce236069999e
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2024-12-02 11:25:28 +0700

    remove cache

--- app/Livewire/Navbar.php
+++ app/Livewire/Navbar.php
@@ -5,7 +5,6 @@ namespace App\Livewire;
 use App\Models\Navigation;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
-use Illuminate\Support\Facades\Cache;
 
 class Navbar extends Component
 {
@@ -14,11 +13,8 @@ class Navbar extends Component
 
     public function mount()
     {
-        $this->role = Auth::check() ? Auth::user()->role : 'guest';
-
-        $this->menus = Cache::remember('menus_' . $this->role, now()->addMinutes(30), function () {
-            return Navigation::whereJsonContains('roles', $this->role)->get();
-        });
+        $this->role = Auth::user()->role;
+        $this->menus = Navigation::whereJsonContains('roles', $this->role)->get();
     }
 
     public function render()
