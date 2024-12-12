875323c6418f01413f242399219a86d09131d308
GilangAddh <gilangadhiperkasa@gmail.com>
2024-12-12 11:49:55 +0700

    logic insert and update

--- app/Livewire/DetailPemetaan.php
+++ app/Livewire/DetailPemetaan.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Models\IndikatorStandarAudit;
+use App\Models\PemetaanStandarAudit;
 use App\Models\StandarAudit;
 use App\Models\User;
 use Livewire\Component;
@@ -10,9 +12,13 @@ class DetailPemetaan extends Component
 {
     public $id_unit = '';
     public $profile_name = '';
+    public $nama_standar = '';
     public $isModalOpen = false;
     public $modalTitle = '';
     public $recordId = null;
+    public $indikator = [];
+    public $selectedStandar = [];
+    public $existingStandar = [];
 
 
     public function mount($id)
@@ -20,6 +26,12 @@ class DetailPemetaan extends Component
         $this->id_unit = $id;
         $unit = User::findOrFail($this->id_unit);
         $this->profile_name = $unit->profile_name;
+
+        $this->existingStandar = PemetaanStandarAudit::where('id_user', $this->id_unit)
+            ->pluck('id_standar')
+            ->toArray();
+
+        $this->selectedStandar = $this->existingStandar;
     }
 
     public function render()
@@ -28,5 +40,46 @@ class DetailPemetaan extends Component
         $unit = User::where('id', '=', $this->id_unit);
 
         return view('livewire.detail-pemetaan', ['standar' => $standar, 'unit' => $unit]);
+    }
+
+    public function openModal($title, $recordId = null)
+    {
+        $this->resetModal();
+        $this->modalTitle = "Indikator Standar {$title}";
+
+        if ($recordId) {
+            $this->recordId = $recordId;
+            $this->loadRecordData();
+        }
+
+        $this->isModalOpen = true;
+    }
+
+    public function resetModal()
+    {
+        $this->reset(['isModalOpen', 'modalTitle', 'recordId']);
+    }
+
+    private function loadRecordData()
+    {
+        $this->indikator = IndikatorStandarAudit::where('id_standar', $this->recordId)->get();
+    }
+
+    public function save()
+    {
+        if (empty($this->selectedStandar)) {
+            session()->flash('message', 'Tidak ada standar yang dipilih.');
+            return redirect()->route('pemetaan-standar-audit');
+        }
+        PemetaanStandarAudit::where('id_user', $this->id_unit)->delete();
+
+        foreach ($this->selectedStandar as $standarId) {
+            PemetaanStandarAudit::create([
+                'id_user' => $this->id_unit,
+                'id_standar' => $standarId,
+            ]);
+        }
+
+        return redirect()->route('pemetaan-standar-audit')->with('success', 'Pemetaan berhasil disimpan.');
     }
 }
--- resources/views/livewire/detail-pemetaan.blade.php
+++ resources/views/livewire/detail-pemetaan.blade.php
@@ -30,28 +30,66 @@
                     <tr>
                         <td class="text-center">{{ $index + 1 }}</td>
                         <td>{{ $item->nama_standar }}</td>
-                        <td><input type="checkbox" class="checkbox mr-2"></td>
+                        <td><input type="checkbox" class="checkbox mr-2" wire:model="selectedStandar"
+                                value="{{ $item->id }}"
+                                {{ in_array($item->id, $existingStandar) ? 'checked' : '' }}></td>
                         <th class="shadow-xl text-center">
-                            <button wire:click="">
+                            <button wire:click="openModal('{{ $item->nama_standar }}',{{ $item->id }})">
                                 <i class="fas fa-eye text-black"></i>
                             </button>
                         </th>
                     </tr>
                 @empty
                     <tr>
-                        <td colspan="3" class="text-center">Tidak ada data yang ditemukan.</td>
+                        <td colspan="4" class="text-center">Tidak ada data yang ditemukan.</td>
                     </tr>
                 @endforelse
             </tbody>
         </table>
 
     </div>
+    <div class="my-4">
+        <a class="btn btn-sm btn-outline text-[#60c0d0] border-[#60c0d0] hover:bg-[#60c0d0] hover:text-white
+            hover:border-none"
+            href="{{ route('pemetaan-standar-audit') }}">Kembali</a>
+        <button
+            class="btn btn-sm btn-outline text-[#60c0d0] border-[#60c0d0] hover:bg-[#60c0d0] hover:text-white
+            hover:border-none"
+            wire:click="save">Simpan</button>
+
+    </div>
     <dialog class="modal" @if ($isModalOpen) open @endif>
         <div class="modal-box w-full max-w-2xl">
             <h3 class="text-lg font-bold mb-4">{{ $modalTitle }}</h3>
             <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" wire:click="resetModal">✕</button>
+            <hr>
+            <table class="table table-zebra table-pin-cols">
+                <thead class="bg-[#60c0d0] text-white font-bold">
+                    <tr class="text-md text-center">
+                        <td class="text-center">No</td>
+                        <td>Nomor Pertanyaan</td>
+                        <td>Pertanyaan</td>
 
-
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($indikator as $index => $item)
+                        <tr class="text-center">
+                            <td>{{ $index + 1 }}.</td>
+                            <td>
+                                {{ $item->nomer_pertanyaan_standar }}
+                            </td>
+                            <td class="max-w-64 text-justify">
+                                {{ $item->pertanyaan_standar }}
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="8" class="text-center">Tidak ada data yang ditemukan.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
     </dialog>
 </div>
