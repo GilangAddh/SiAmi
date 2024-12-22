ea0eaee0762fc40585d35d43485f0b8eccf7e5d5
Bintang Fajar Julio <79626613+bintangfjulio@users.noreply.github.com>
2024-12-22 22:53:08 +0700

    add sorting

--- app/Livewire/PenugasanAudit.php
+++ app/Livewire/PenugasanAudit.php
@@ -37,8 +37,16 @@ class PenugasanAudit extends Component
                 'periode_audit.tanggal_mulai',
                 'periode_audit.tanggal_akhir'
             )
+            ->when($this->sortStatus, function ($query) {
+                if ($this->sortStatus == "sudah") {
+                    return $query->orderByRaw('CASE WHEN (SELECT COUNT(DISTINCT penugasan_audit.id_auditor) FROM penugasan_audit WHERE penugasan_audit.id_unit = jadwal_audit.id_unit AND penugasan_audit.id_periode = jadwal_audit.id_periode) > 0 THEN 1 ELSE 0 END DESC');
+                } else {
+                    return $query->orderByRaw('CASE WHEN (SELECT COUNT(DISTINCT penugasan_audit.id_auditor) FROM penugasan_audit WHERE penugasan_audit.id_unit = jadwal_audit.id_unit AND penugasan_audit.id_periode = jadwal_audit.id_periode) > 0 THEN 1 ELSE 0 END ASC');
+                }
+            })
             ->orderBy('periode_audit.tanggal_mulai', 'desc')
             ->orderBy('periode_audit.tanggal_akhir', 'desc')
+            ->with(['periodeAudit', 'unitKerja', 'standarAudit'])
             ->when($this->id_periode, function ($query) {
                 return $query->where('jadwal_audit.id_periode', $this->id_periode);
             })
